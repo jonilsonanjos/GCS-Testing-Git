ef241bd29451d5a2447d4d8f4a9ce44815813827
Jonilson Coelho <jcda@icomp.ufam.edu.br>
2022-11-10 16:58:02 -0400

    instalação do pacote morgan ao projeto para ver os logs de acessos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
-require('dotenv').config();
+import express from "express"; // nova forma padrão do ESMscript para importar no nodejs
+import morgan from "morgan" // para mostrar ao desenvolvedor os logs de acesso à API.
+//require('dotenv').config();
 
 const app = express();
+app.use(morgan("combined"));
+
 const PORT = process.env.PORT || 8855;
 
 app.get('/',(req,res)=>{
